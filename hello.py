291a47019fcb1b90a8acf3639835449c285e9f91
zhiyong-lv <zhiyong_lv1983@outlook.com>
2019-05-21 23:29:11 +0800

    Add session and form

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
-# from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask import session, redirect, url_for, flash
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
 
 
 @app.errorhandler(404)
@@ -20,9 +29,19 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    # first time, will return false, because client will send a GET request
+    # instead of a POST request
+    if form.validate_on_submit():
+        old_name = session['name']
+        session['name'] = form.name.data
+        if old_name is not None and session['name'] != old_name:
+            flash("You change login account")
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session['name'])
 
 
 @app.route('/user/<name>')
